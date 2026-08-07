3dddba04166042d3bc725d9db1d2a17a6d873259
federicoosler <fosler@fi.uba.ar>
2026-08-07 12:40:26 -0300

    Fix: Agrego favicon, corrijo enlaces y error en contacto.js

--- frontend/services/email.py
+++ frontend/services/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
+import os
 
 def enviar_email(nombre,mensajero_email,mensaje,mail):
-    msg = Message('Nuevo mensaje de contacto', sender='federicoosler549@gmail.com', recipients=['federicoosler549@gmail.com'])
-    msg.body = f'De:{nombre},{mensajero_email} \n Mensaje:{mensaje}'
+    msg = Message('Nuevo mensaje de contacto', sender=os.getenv('MAIL_SENDER'), recipients=[os.getenv('MAIL_RECEIVER')])
+    msg.body = f'De: {nombre}, {mensajero_email} \n Mensaje: {mensaje}'
     mail.send(msg)
--- frontend/static/js/contacto.js
+++ frontend/static/js/contacto.js
@@ -1,7 +1,10 @@
 const msg = document.querySelector('.mensaje-enviado');
-setTimeout(() => {
-  msg.classList.add('oculto');
+
+if(msg){
   setTimeout(() => {
-    msg.style.display = 'none';
-  }, 300);
-}, 3000);
+    msg.classList.add('oculto');
+    setTimeout(() => {
+      msg.style.display = 'none';
+    }, 300);
+  }, 3000);
+};
